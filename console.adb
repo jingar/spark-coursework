4f2ffc4f93abbb109ae317053ad2a8ca6f165b64
saad <sa521@hw.ac.uk>
2013-10-21 16:55:17 +0100

    finished console package body

---
+++ console.adb
@@ -0,0 +1,42 @@
+package body Console
+is
+   Reset_Status: Boolean:= False;
+   SPAD_Cnt: Integer:= 0;
+   
+   procedure Enable_Reset
+   is
+   begin
+      Reset_Status:= True;
+   end Enable_Reset;
+   
+   procedure Disable_Reset
+   is 
+   begin
+      Reset_Status:= True;
+   end Disable_Reset;
+   
+   function Reset_Enabled return Boolean
+   is
+   begin
+      return Reset_Status;
+   end Reset_Enabled;
+   
+   procedure Inc_SPAD_Cnt
+   is
+   begin
+      Spad_Cnt:= Spad_Cnt + 1;
+   end Inc_SPAD_Cnt;
+   
+   procedure Reset_SPAD_Cnt
+   is
+   begin
+      Spad_Cnt:= 0;
+   end Reset_SPAD_Cnt;
+   
+   function SPAD_Cnt_Value return Integer
+   is
+   begin
+      return SPAD_Cnt;
+   end SPAD_Cnt_Value;
+end Console;
+
